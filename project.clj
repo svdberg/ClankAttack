b87781862a1852b3a47b05dcc1e142669a627c44
Maurits <maurits.rijk@gmail.com>
2011-02-11 20:51:35 +0100

    updated to Clojure 1.3alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ClankAttack "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [org.clojure/clojure-contrib "1.2.0"]])
