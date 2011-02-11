e5db90774a1549244f1e9a3a8922fbc52ca2187b
Sander van den Berg <svdberg@me.com>
2011-02-11 15:30:56 +0100

    initial commit

---
+++ src/ClankAttack/core.clj
@@ -0,0 +1,1 @@
+(ns ClankAttack.core)
---
+++ test/ClankAttack/test/core.clj
@@ -0,0 +1,6 @@
+(ns ClankAttack.test.core
+  (:use [ClankAttack.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
