79e5a1d145631f77556c21c20a1b43a76802150e
Maurits <maurits.rijk@gmail.com>
2011-02-12 18:20:18 +0100

    added couple of little tankies

--- src/ClankAttack/core.clj
+++ src/ClankAttack/core.clj
@@ -7,7 +7,7 @@
 ; bunch of constants
 (def *field-width* 500)
 (def *field-height* 500)
-(def *tank-radius* 10)
+(def *tank-radius* 20)
 
 (defrecord Tank [x y id angle])
 
@@ -22,19 +22,35 @@
   [n]
   (repeatedly n create-tank))
 
-(defn render [g]
+(defn render-tank
+  "Render a single tank"
+  [g tank]
+  (doto g
+    (.setColor (Color/green))
+    (.fillOval (:x tank) (:y tank) *tank-radius* *tank-radius*)))
+
+(defn render-background [g img]
+  (doto g
+    (.setColor (Color. 255 230 255))
+    (.fillRect 0 0 (.getWidth img) (.getHeight img))))
+
+(defn render-tanks
+  "Render all tanks"
+  [g tanks]
+  (dorun (map #(render-tank g %) tanks)))
+
+(defn render [g tanks]
   (let [img (new BufferedImage *field-width* *field-height*
                  (. BufferedImage TYPE_INT_ARGB))
         bg (.getGraphics img)]
-    (doto bg
-      (.setColor (Color. 255 230 255))
-      (.fillRect 0 0 (.getWidth img) (.getHeight img)))
+    (render-background bg img)
+    (render-tanks bg tanks)
     (.drawImage g img 0 0 nil)
     (.dispose bg)))
 
 (defn create-panel [tanks]
   (doto (proxy [JPanel] []
-          (paint [g] (render g)))
+          (paint [g] (render g tanks)))
     (.setPreferredSize (new Dimension *field-width* *field-height*))))
 
 (defn draw-battle-field [tanks]
